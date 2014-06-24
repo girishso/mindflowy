b8c9d34c5eba93f7f121adb7341117deec8b564b
Prelang Builder <builder@prelang.com>
2014-06-24 05:21:43 +0000

    Injected Google Analytics tracker ID configuration into development configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ MindFlowy::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #->Prelang
+  GA.tracker = "DDDDD"
+
 end
+

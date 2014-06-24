82da48e47a707c61dc4db3f6bee224938c7faea0
Prelang Builder <builder@prelang.com>
2014-06-24 05:21:35 +0000

    Overrode Devise routes for new stylized paths

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,17 @@ MindFlowy::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  #->Prelang (user_login:devise/stylized_paths)
+  devise_scope :user do
+    get    "login"   => "devise/sessions#new",         as: :new_user_session
+    post   "login"   => "devise/sessions#create",      as: :user_session
+    delete "signout" => "devise/sessions#destroy",     as: :destroy_user_session
+    
+    get    "signup"  => "devise/registrations#new",    as: :new_user_registration
+    post   "signup"  => "devise/registrations#create", as: :user_registration
+    put    "signup"  => "devise/registrations#update", as: :update_user_registration
+    get    "account" => "devise/registrations#edit",   as: :edit_user_registration
+  end
+
 end

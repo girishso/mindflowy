fe02b47a931b333c137999673cdb156b0f2d3e3a
Prelang Builder <builder@prelang.com>
2014-06-24 05:21:28 +0000

    Defined User.find_for_facebook_oauth to find or create a User based on their Facebook Oauth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
+
+  #->Prelang (user_login/devise)
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+
+    # The User was found in our database
+    return user if user
+
+    # The User was not found and we need to create them
+    User.create(name:     auth.extra.raw_info.name,
+                provider: auth.provider,
+                uid:      auth.uid,
+                email:    auth.info.email,
+                password: Devise.friendly_token[0,20])
+  end
+
+
 end

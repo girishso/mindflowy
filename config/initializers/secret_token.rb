416dfaa56ac7a89e5037eae7ecbca9d901571f24
Prelang Builder <builder@prelang.com>
2014-06-24 05:20:43 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MindFlowy::Application.config.secret_key_base = '08e1b0825148371991bc1fc5359126bcc0a53aba04654db28cf1f74fd531dee3d7f15669cb7ab589667e97e5df682ee01f41e988d5c65f5a5286d51e70093dd1'

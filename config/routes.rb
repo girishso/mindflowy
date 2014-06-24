f3d3a0f3ff6bb929ba7f27970d5e65ed1b1fe89e
Prelang Builder <builder@prelang.com>
2014-06-24 05:21:46 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MindFlowy::Application.routes.draw do
+  get "landings/index"
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

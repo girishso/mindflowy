520b286d0133a07dc318cb92ca8b7fc24d9c98d4
Girish S <girish.sonawane@gmail.com>
2014-06-27 13:02:06 +0530

    configured pg gem

--- Gemfile
+++ Gemfile
@@ -5,14 +5,13 @@ ruby "2.1.1"
 gem 'rails', '4.0.4'
 
 group :development do
-  # Use sqlite3 as the database for Active Record
-  gem 'sqlite3'
 end
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+gem "pg"
 
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140624061304) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "nodes", force: true do |t|
     t.string  "title"
     t.text    "description"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20140624061304) do
     t.string  "ancestry"
   end
 
-  add_index "nodes", ["ancestry"], name: "index_nodes_on_ancestry"
-  add_index "nodes", ["position"], name: "index_nodes_on_position"
+  add_index "nodes", ["ancestry"], name: "index_nodes_on_ancestry", using: :btree
+  add_index "nodes", ["position"], name: "index_nodes_on_position", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20140624061304) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end

abbaea08d27f478c864d65b38a79efda9c7878f6
Girish S <girish.sonawane@gmail.com>
2014-07-07 15:20:49 +0530

    show nodes if user signed in

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,6 +1,7 @@
 class LandingsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    redirect_to landings_show_path if user_signed_in?
   end
 end

316030085a34fc0396d8a72765a6edb1c4c14bba
Girish S <girish.sonawane@gmail.com>
2014-07-11 13:27:23 +0530

    got rid of landings/show

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,6 +2,5 @@ class LandingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    redirect_to landings_show_path if user_signed_in?
   end
 end
